78009e4ea2ae5f3226cbb6bcedd82b5926ac7a74
Muhamad Fikri <muhamad.fikri@unida.ac.id>
2021-12-20 05:48:51 +0700

    Perulangan Tunggal

--- src/Javafor.java
+++ src/Javafor.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
 public class Javafor {
-	public static void main(String[] args) { int i;
-	for (i = 0; i < 7; i++) { 
-		System.out.println("Hello World Ke-" +i);
-	}
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+
+		int pilihan, awal, akhir;
+
+		System.out.println("PROGRAM MENAMPILKAN DERET BILANGAN GANJIL");
+		System.out.println("-------------------------------------------------");
+		System.out.print("Masukan Pilihan [BILANGAN Ganjil]: ");
+		pilihan=input.nextInt();
+		System.out.print("Masukan Nilai Awal : ");
+		awal=input.nextInt();
+		System.out.print("Masukan Nilai Akhir : ");
+		akhir=input.nextInt();
+		System.out.println("--------------------------- ");
+			System.out.println("Deret Bilangan Ganjil");
+			for(int i=awal;i<=akhir;i++){
+				if (i%2!=0)
+					System.out.print(i+" ");
+			} 
+		System.out.println("");
 	}
 }
